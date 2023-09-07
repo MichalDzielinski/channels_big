51910d9609652f931f3a9d321a2236159c406f64
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-09-07 18:49:32 +0200

    Another

--- static/js/main_admin.js
+++ static/js/main_admin.js
@@ -3,7 +3,10 @@
  */
 
 const chatRoom = document.querySelector('#room_uuid').textContent.replaceAll('"', '')
+
 let chatSocket = null
+
+
 /**
  * Elements
  */
@@ -16,76 +19,86 @@ const chatSubmitElement = document.querySelector('#chat_message_submit')
 /**
  * Functions
  */
-function onChatMessage(data){
-    console.log('Bazinga');
-    console.log('onChatMessage', data)
 
-    if (data.type == 'chat_message'){  
-        if(!data.agent){
-            `<div class='flex w-full mt-2 space-x-3 max-w-md'>
-                
-                
-                <div>
-                    <div class='bg-gray-600 p-3 rounded-l-lg rounded-br-lg'>
-                        <p class='text-sm'>${data.message}</p>
-                    </div>
-            
-            
-                    <span class='text-xs text-gray-500 leading-none'>${data.created_at} ago</span>
-                </div>
-                
-                <div class='flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2'>${data.initials}</div>
-            </div>`
-            
-
-        }else{
-            chatLogElement.innerHTML += 
-            `<div class='flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end'>
-                
-                
-                <div>
-                    <div class='bg-blue-600 p-3 rounded-l-lg rounded-br-lg'>
-                        <p class='text-sm'>${data.message}</p>
-                    </div>
-            
-            
-                    <span class='text-xs text-gray-500 leading-none'>${data.created_at} ago</span>
-                </div>
-                
-                <div class='flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2'>${data.initials}</div>
-            </div>`
-        }
-    }
-    scrollToBottom()
+function scrollToBottom() {
+    chatLogElement.scrollTop = chatLogElement.scrollHeight
 }
 
-function sendMessage(){
+
+function sendMessage() {
     chatSocket.send(JSON.stringify({
         'type': 'message',
         'message': chatInputElement.value,
-        'name': document.querySelector('#user_name').textContent.replaceAll('"',''),
-        'agent': document.querySelector('#user_id').textContent.replaceAll('"',''),
+        'name': document.querySelector('#user_name').textContent.replaceAll('"', ''),
+        'agent': document.querySelector('#user_id').textContent.replaceAll('"', ''),
     }))
 
     chatInputElement.value = ''
 }
 
-chatSubmitElement.onclick = function(e){
-    e.preventDefault()
-    console.log('Buuz')
-    sendMessage()
 
-    return false
-}
+function onChatMessage(data) {
+    console.log('onChatMessage', data)
 
-chatInputElement.onkeyup = function(e){
-    if (e.keyCode == 13){
-        sendMessage()
+    if (data.type == 'chat_message') {
+        let tmpInfo = document.querySelector('.tmp-info')
+
+        if (tmpInfo) {
+            tmpInfo.remove()
+        }
+        
+        if (!data.agent) {
+            chatLogElement.innerHTML += `
+                <div class="flex w-full mt-2 space-x-3 max-w-md">
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+
+                    <div>
+                        <div class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
+                            <p class="text-sm">${data.message}</p>
+                        </div>
+                        
+                        <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
+                    </div>
+                </div>
+            `
+        } else {
+            chatLogElement.innerHTML += `
+                <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
+                    <div>
+                        <div class="bg-blue-300 p-3 rounded-l-lg rounded-br-lg">
+                            <p class="text-sm">${data.message}</p>
+                        </div>
+                        
+                        <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
+                    </div>
+
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+                </div>
+            `
+        }
+    } else if (data.type == 'writing_active') {
+        if (!data.agent) {
+            let tmpInfo = document.querySelector('.tmp-info')
+
+            if (tmpInfo) {
+                tmpInfo.remove()
+            }
+
+            chatLogElement.innerHTML += `
+                <div class="tmp-info flex w-full mt-2 space-x-3 max-w-md">
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+
+                    <div>
+                        <div class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
+                            <p class="text-sm">The client is typing...</p>
+                        </div>
+                    </div>
+                </div>
+            `
+        }
     }
-}
 
-function scrollToBottom(){
-    chatLogElement.scrollTop = chatLogElement.scrollHeight
+    scrollToBottom()
 }
 
 
@@ -95,16 +108,48 @@ function scrollToBottom(){
 
 chatSocket = new WebSocket(`ws://${window.location.host}/ws/${chatRoom}/`)
 
-chatSocket.onmessage = function(e){
+chatSocket.onmessage = function(e) {
     console.log('on message')
 
     onChatMessage(JSON.parse(e.data))
 }
 
-chatSocket.onopen = function(e){
+chatSocket.onopen = function(e) {
     console.log('on open')
+
+    scrollToBottom()
 }
 
-chatSocket.onclose = function(e){
-    console.log('on close')
+chatSocket.onclose = function(e) {
+    console.log('chat socket closed unexpectadly')
+}
+
+
+/**
+ * Event listeners
+ */
+
+chatSubmitElement.onclick = function(e) {
+    e.preventDefault()
+
+    sendMessage()
+
+    return false
+}
+
+
+chatInputElement.onkeyup = function(e) {
+    if (e.keyCode == 13) {
+        sendMessage()
+    }
+}
+
+
+chatInputElement.onfocus = function(e) {
+    chatSocket.send(JSON.stringify({
+        'type': 'update',
+        'message': 'writing_active',
+        'name': document.querySelector('#user_name').textContent.replaceAll('"', ''),
+        'agent': document.querySelector('#user_id').textContent.replaceAll('"', ''),
+    }))
 }
